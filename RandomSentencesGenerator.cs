8e1183e22c17cf5d52588b28bd6cdf622f63e3c9
peterdidimitrov <petr_dimitrov@abv.bg>
2023-01-03 20:28:19 +0200

    Add new project Random Sentences Generator

---
+++ RandomSentencesGenerator.cs
@@ -0,0 +1,51 @@
+﻿namespace RandomSentencesGenerator
+{
+    using System;
+
+    internal class RandomSentencesGenerator
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Peter", "Michell", "Jane", "Steve" };
+            string[] places = { "Sofia", "Plovdiv", "Varna", "Burgas" };
+            string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
+            string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
+            string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
+            string[] details = { "near the river", "at home", "in the park" };
+
+            Console.WriteLine("Hello, this is your first ramdom-generated sentence: ");
+
+
+            while (true)
+            {
+                string randomName = GetRandomWord(names);
+                string randomPlace = GetRandomWord(places);
+                string randomVerb = GetRandomWord(verbs);
+                string randomNoun = GetRandomWord(nouns);
+                string randomAdverb = GetRandomWord(adverbs);
+                string randomDetail = GetRandomWord(details);
+
+                string who = $"{randomName} from {randomPlace}";
+                string action = $"{randomAdverb} {randomVerb} {randomNoun}";
+                string sentence = $"{who} {action} {randomDetail}.";
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(sentence);
+                Console.ResetColor();
+
+                Console.WriteLine("Click [Enter] to generate a new one.");
+
+                Console.ReadLine();
+
+            }
+        }
+
+        static string GetRandomWord(string[] words)
+        {
+            Random random = new Random();
+            int ranndomIndex = random.Next(words.Length);
+            string word = words[ranndomIndex];
+            return word;
+        }
+    }
+}
