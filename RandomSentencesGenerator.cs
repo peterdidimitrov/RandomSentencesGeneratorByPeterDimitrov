a2c9d2898486c3a851c4ee04eb0d712433dd498e
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-01-05 13:32:38 +0200

    Makes the code more readable

--- RandomSentencesGenerator.cs
+++ RandomSentencesGenerator.cs
@@ -6,37 +6,43 @@
     {
         static void Main(string[] args)
         {
+            //Initialising the arrays of words
             string[] names = { "Peter", "Michell", "Jane", "Steve" };
             string[] places = { "Sofia", "Plovdiv", "Varna", "Burgas" };
             string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
             string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
             string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
             string[] details = { "near the river", "at home", "in the park" };
-
+            
+            //The program displays greeting
             Console.WriteLine("Hello, this is your first ramdom-generated sentence: ");
 
 
             while (true)
             {
+                //Simplifying the code by creating a method, which chooses the random word
+                //Declaring the variables for different part of the sentence
                 string randomName = GetRandomWord(names);
                 string randomPlace = GetRandomWord(places);
                 string randomVerb = GetRandomWord(verbs);
                 string randomNoun = GetRandomWord(nouns);
                 string randomAdverb = GetRandomWord(adverbs);
                 string randomDetail = GetRandomWord(details);
-
+                
+                //Creating a random sentence
                 string who = $"{randomName} from {randomPlace}";
                 string action = $"{randomAdverb} {randomVerb} {randomNoun}";
                 string sentence = $"{who} {action} {randomDetail}.";
 
+                //Displaying the sentence in green colour
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(sentence);
                 Console.ResetColor();
 
-                Console.WriteLine("Click [Enter] to generate a new one.");
+                //The program restarts and generates another sentence by pressing ENTER button
+                Console.WriteLine("Click [ENTER] to generate a new one.");
 
                 Console.ReadLine();
-
             }
         }
 
